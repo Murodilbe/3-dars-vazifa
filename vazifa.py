5a743450a51b3c842452535bafd752c42c618d05
Murodilbe <murodilxolmatov73@gmail.com>
2024-03-01 19:06:37 +0500

    raqam buyicha tekshirildi

--- vazifa.py
+++ vazifa.py
@@ -1,6 +1,5 @@
 def add_user(name, lastname, age, address, email, phone):
     users.append((name, lastname, age, address, email, phone))
-
 users = []
 while True:
     name = input("Ismingizni kiriting: ")
@@ -8,18 +7,14 @@ while True:
     age = input("Yoshingizni kiriting: ")
     address = input("Manzilingizni kiriting: ")
     email = input("Emailingizni kiriting: ")
-    if phone[4] == "+998" or len(phone[14]):
-        phone = input("Telefon raqamingizni kiriting: ")
-
-
-
+    phone = input("Telefon raqamingizni kiriting: ")
+    if phone[::4] == "+998" or len(phone[14]):
+        pass
+    else:
+        break
     add_user(name, lastname, age, address, email, phone)
-
     stop = input("Dasturni to'xtatish uchun 'stop' deb yozasizmi: ")
     if stop.lower() == "stop":
         break
-
-
-
 for user in users:
     print(users)
