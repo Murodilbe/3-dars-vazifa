cf0a722c037a4e393a3b42a2f917317f46491eee
Murodilbe <murodilxolmatov73@gmail.com>
2024-03-01 18:56:49 +0500

    telefon raqam tekshirildi shart bo'yicha

---
+++ vazifa.py
@@ -0,0 +1,25 @@
+def add_user(name, lastname, age, address, email, phone):
+    users.append((name, lastname, age, address, email, phone))
+
+users = []
+while True:
+    name = input("Ismingizni kiriting: ")
+    lastname = input("Familiyangizni kiriting: ")
+    age = input("Yoshingizni kiriting: ")
+    address = input("Manzilingizni kiriting: ")
+    email = input("Emailingizni kiriting: ")
+    if phone[4] == "+998" or len(phone[14]):
+        phone = input("Telefon raqamingizni kiriting: ")
+
+
+
+    add_user(name, lastname, age, address, email, phone)
+
+    stop = input("Dasturni to'xtatish uchun 'stop' deb yozasizmi: ")
+    if stop.lower() == "stop":
+        break
+
+
+
+for user in users:
+    print(users)
